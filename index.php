ff38edafffb780c1c6b76055f0ebaf87346e5ee6
diogogoebel <diogo.goebel@gmail.com>
2016-01-13 14:39:02 -0200

    green

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<p>Olá, eu sou o Hulk Azul, estou no POD: <?php echo getenv("SERVER_ADDR"); ?></p>
-<img src="hulk-blue.jpg">
+<p>Olá, eu sou o Hulk Green, estou no POD: <?php echo getenv("SERVER_ADDR"); ?></p>
+<img src="hulk-green.jpg">
